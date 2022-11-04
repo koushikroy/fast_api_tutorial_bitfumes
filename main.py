20b8505be2a04be5ec9f3945d6523b8c2bfb2049
Koushik Roy <36086890+koushikroy@users.noreply.github.com>
2022-11-04 17:19:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import imp
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {'name': 'Koushik'}}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data':id}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'All unpublished blog'}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data':{'1', '2'}}
